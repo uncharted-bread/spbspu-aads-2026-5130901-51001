f68413c68b3f2f12df58acd3eec106aec33de1d3
uncharted-bread <gordeychikmika@gmail.com>
2026-06-22 21:11:03 +0300

    added more test for backpack algorithm

--- gordejchik.mikhail/F0/test-backpack.cpp
+++ gordejchik.mikhail/F0/test-backpack.cpp
@@ -46,4 +46,44 @@ BOOST_AUTO_TEST_CASE(singleCardTooExpensive)
   BOOST_TEST(res.totalPower_ == 0);
 }
 
+BOOST_AUTO_TEST_CASE(classicBackpack)
+{
+  Card c1{"a", 6, 1, "", ""};
+  Card c2{"b", 10, 2, "", ""};
+  Card c3{"c", 12, 3, "", ""};
+  Card* arr[] = {&c1, &c2, &c3};
+  BackpackResult res = solveBackpack(arr, 3, 5);
+
+  BOOST_TEST(res.totalPower_ == 22);
+  BOOST_TEST(res.count_ == 2u);
+  freeBackpackResult(res);
+}
+
+BOOST_AUTO_TEST_CASE(allCardsFit)
+{
+  Card c1{"x", 5, 2, "", ""};
+  Card c2{"y", 3, 1, "", ""};
+  Card c3{"z", 7, 3, "", ""};
+  Card* arr[] = {&c1, &c2, &c3};
+  BackpackResult res = solveBackpack(arr, 3, 100);
+
+  BOOST_TEST(res.count_ == 3u);
+  BOOST_TEST(res.totalPower_ == 15);
+  freeBackpackResult(res);
+}
+
+BOOST_AUTO_TEST_CASE(greedyWouldFail)
+{
+  Card c1{"cheap", 8, 4, "", ""};
+  Card c2{"mid", 9, 5, "", ""};
+  Card c3{"combo1", 5, 3, "", ""};
+  Card c4{"combo2", 5, 3, "", ""};
+  Card* arr[] = {&c1, &c2, &c3, &c4};
+  BackpackResult res = solveBackpack(arr, 4, 6);
+
+  BOOST_TEST(res.totalPower_ == 10);
+  BOOST_TEST(res.count_ == 2u);
+  freeBackpackResult(res);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
