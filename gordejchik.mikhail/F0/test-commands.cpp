fd71d3ebc8cab461db01006a543fab11ecc3a0de
Michael <gordeychikmika@gmail.com>
2026-07-19 01:30:23 +0300

    Add session save and load tests

--- gordejchik.mikhail/F0/test-commands.cpp
+++ gordejchik.mikhail/F0/test-commands.cpp
@@ -325,3 +325,104 @@ BOOST_AUTO_TEST_CASE(dominatesUnknownType)
 }
 
 BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(SessionSuite)
+
+BOOST_AUTO_TEST_CASE(sessionRoundTripRestoresState)
+{
+  DeckStore decks;
+  gordejchik::game_config_t config{false, gordejchik::DEFAULT_BONUS_VALUE, {}};
+  std::ostringstream out;
+  gordejchik::cmdCreate(decks, config, parseLine("create \"Light Side\""), out);
+  gordejchik::cmdAdd(decks, config, parseLine("add \"Light Side\" Yoda 10 8"), out);
+  gordejchik::cmdSetType(decks, config, parseLine("set-type \"Light Side\" Yoda Jedi"), out);
+  gordejchik::cmdCreate(decks, config, parseLine("create empty"), out);
+  gordejchik::cmdConfig(decks, config, parseLine("config bonus on"), out);
+  gordejchik::cmdConfig(decks, config, parseLine("config bonus 5"), out);
+  gordejchik::cmdConfig(decks, config, parseLine("config rule Jedi Sith"), out);
+
+  const std::string filename = "f0-session-test.txt";
+  gordejchik::cmdSave(decks, config, parseLine("save " + filename), out);
+
+  DeckStore restoredDecks;
+  gordejchik::game_config_t restored{false, gordejchik::DEFAULT_BONUS_VALUE, {}};
+  gordejchik::cmdLoad(restoredDecks, restored, parseLine("load " + filename), out);
+  std::remove(filename.c_str());
+
+  BOOST_TEST(out.str().empty());
+  BOOST_TEST(restored.bonusEnabled);
+  BOOST_TEST(restored.bonusValue == 5);
+  BOOST_TEST(restored.rules.size() == 1u);
+  BOOST_TEST(restored.rules.contains("Jedi>Sith"));
+  BOOST_TEST(restoredDecks.size() == 2u);
+  BOOST_REQUIRE(restoredDecks.contains("Light Side"));
+  BOOST_TEST(restoredDecks.contains("empty"));
+  BOOST_TEST(restoredDecks.at("empty").empty());
+  const gordejchik::Deck& deck = restoredDecks.at("Light Side");
+  BOOST_REQUIRE(deck.contains("Yoda"));
+  BOOST_TEST(deck.at("Yoda").power == 10);
+  BOOST_TEST(deck.at("Yoda").cost == 8);
+  BOOST_TEST(deck.at("Yoda").type == "Jedi");
+}
+
+BOOST_AUTO_TEST_CASE(sessionLoadReplacesState)
+{
+  DeckStore decks;
+  gordejchik::game_config_t config{false, gordejchik::DEFAULT_BONUS_VALUE, {}};
+  std::ostringstream out;
+  const std::string filename = "f0-replace-test.txt";
+  gordejchik::cmdSave(decks, config, parseLine("save " + filename), out);
+
+  gordejchik::cmdCreate(decks, config, parseLine("create old"), out);
+  gordejchik::cmdConfig(decks, config, parseLine("config rule Old Rule"), out);
+  gordejchik::cmdLoad(decks, config, parseLine("load " + filename), out);
+  std::remove(filename.c_str());
+
+  BOOST_TEST(out.str().empty());
+  BOOST_TEST(decks.empty());
+  BOOST_TEST(config.rules.empty());
+}
+
+BOOST_AUTO_TEST_CASE(corruptedSessionKeepsState)
+{
+  const std::string filename = "f0-corrupt-test.txt";
+  {
+    std::ofstream file(filename);
+    file << "SESSION\non -3\n0\n0\n";
+  }
+  DeckStore decks;
+  gordejchik::game_config_t config{false, gordejchik::DEFAULT_BONUS_VALUE, {}};
+  std::ostringstream out;
+  gordejchik::cmdCreate(decks, config, parseLine("create keepme"), out);
+  gordejchik::cmdLoad(decks, config, parseLine("load " + filename), out);
+  std::remove(filename.c_str());
+
+  BOOST_TEST(out.str() == "<INVALID COMMAND>\n");
+  BOOST_TEST(decks.contains("keepme"));
+  BOOST_TEST(!config.bonusEnabled);
+}
+
+BOOST_AUTO_TEST_CASE(deckFileRejectedAsSession)
+{
+  const std::string filename = "f0-deckfile-test.txt";
+  DeckStore decks;
+  gordejchik::game_config_t config{false, gordejchik::DEFAULT_BONUS_VALUE, {}};
+  std::ostringstream out;
+  gordejchik::cmdCreate(decks, config, parseLine("create d"), out);
+  gordejchik::cmdAdd(decks, config, parseLine("add d Yoda 10 8"), out);
+  gordejchik::cmdSave(decks, config, parseLine("save d " + filename), out);
+
+  gordejchik::cmdLoad(decks, config, parseLine("load " + filename), out);
+  BOOST_TEST(out.str() == "<INVALID COMMAND>\n");
+  BOOST_TEST(decks.contains("d"));
+
+  DeckStore other;
+  std::ostringstream out2;
+  gordejchik::cmdSave(other, config, parseLine("save " + filename), out2);
+  gordejchik::cmdLoad(other, config, parseLine("load s " + filename), out2);
+  std::remove(filename.c_str());
+  BOOST_TEST(out2.str() == "<INVALID COMMAND>\n");
+  BOOST_TEST(!other.contains("s"));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
